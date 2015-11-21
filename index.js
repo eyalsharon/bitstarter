ee4586270d5da0bd24d31aed5a8fb5ec4e29052d
Eyal Sharon <ubuntu@ip-172-31-14-43.ec2.internal>
2015-11-21 10:17:30 +0000

    correction to index.js to listen to port

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var express = require('express');
 var fs = require('fs');
+var express = require('express');
+
+var app = express()
 
 var data = fs.readFileSync("index.html", "utf-8");
 
-var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.send(data);
 });
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
