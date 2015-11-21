2a26afd627bfcbd9035e0d2a8cb9eff837ea9b6d
Eyal Sharon <ubuntu@ip-172-31-14-43.ec2.internal>
2015-11-21 09:42:36 +0000

    modify the index.js to resad from index.html

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var data = fs.readFileSync("index.html", "utf-8");
 
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-})
+  response.send(data);
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+
+
