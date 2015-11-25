8b4f3dabf9a7c1ba34b9d81d94ce340036492d02
Eyal Sharon <ubuntu@ip-172-31-14-43.ec2.internal>
2015-11-25 06:54:03 +0000

    edited productdesc in index.html

--- grader.js
+++ grader.js
@@ -1,29 +1,15 @@
-# for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
+#!/usr/bin/env node
 
 var fs = require('fs');
 var program = require('commander');
+var sys = require('util');
+var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -59,14 +45,56 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var cheerioUrlResult = function(urlCheck) {
+    console.log('I am here: ', urlCheck);
+    return cheerio.load(urlCheck);
+};
+
+
+var getUrl = function(url_result, checksfile) {
+$ = cheerioUrlResult(url_result);
+
+var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+    }
+    return out;
+};
+  
+  
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
-    exports.checkHtmlFile = checkHtmlFile;
+        .option('-c, --checks <check_file>', 'checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>' , 'Command Line defined URL')
+
+       .parse(process.argv);
+
+       if(program.url) {
+          console.log(' - url' + "argument");
+          //getUrl(program.url, program.checks);
+          rest.get(program.url).on('complete', function(result) {
+
+          var checkJson = getUrl(result, program.checks);
+          var outJson = JSON.stringify(checkJson, null, 4);
+          //console.log(cheerioUrlResult);
+          
+        });
+
+
+         } else  {
+
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson); 
+              }
+
+  }
+
+else {
+
+        exports.checkHtmlFile = checkHtmlFile;
 }
